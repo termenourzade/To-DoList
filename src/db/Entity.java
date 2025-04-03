3d454da14883dbd0dbc33e759e73e4609a51a535
terme <termenoorzadeh@gmail.com>
2025-04-03 15:46:33 -0700

    adding abstract get entity code method

--- src/db/Entity.java
+++ src/db/Entity.java
@@ -2,6 +2,9 @@ package db;
 
 public abstract class Entity implements Cloneable {
     public int id;
+
+    public abstract int getEntityCode();
+
     @Override
     public Entity clone() throws CloneNotSupportedException {
         return (Entity) super.clone();
