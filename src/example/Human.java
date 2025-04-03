3b4f0f17e2eab9f91608cb01260e36a5c9ad7a9a
terme <termenoorzadeh@gmail.com>
2025-04-03 15:49:07 -0700

    overriding get entity code method in Human class

--- src/example/Human.java
+++ src/example/Human.java
@@ -5,10 +5,16 @@ import db.*;
 public class Human extends Entity implements Cloneable {
     public String name;
     public int age;
+    public static final int HUMAN_ENTITY_CODE = 14;
 
-    public Human(String name) {
+    public Human(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return HUMAN_ENTITY_CODE;
     }
 
     @Override
