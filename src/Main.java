b33f66c52d6c3bdc9b6c154730ca050636b5a248
terme <termenoorzadeh@gmail.com>
2025-04-10 05:27:11 -0700

    adding main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,36 @@
 import db.Database;
-import db.exception.EntityNotFoundException;
 import db.exception.InvalidEntityException;
-import example.Human;
-import example.HumanValidator;
+import example.Document;
+
 
 public class Main {
     public static void main(String[] args) throws InvalidEntityException {
-        Database.registerValidator(Human.HUMAN_ENTITY_CODE, new HumanValidator());
+        Document doc = new Document("Eid Eid Eid");
 
-        Human ali = new Human("Ali", -10);
-        Database.add(ali);
+        Database.add(doc);
+
+        System.out.println("Document added");
+
+        System.out.println("id: " + doc.id);
+        System.out.println("content: " + doc.content);
+        System.out.println("creation date: " + doc.getCreationDate());
+        System.out.println("last modification date: " + doc.getLastModificationDate());
+        System.out.println();
+
+        try {
+            Thread.sleep(30_000);
+        } catch (InterruptedException e) {
+            System.out.println("Sleep interrupted!");
+        }
+
+        doc.content = "This is the new content";
+
+        Database.update(doc);
+
+        System.out.println("Document updated");
+        System.out.println("id: " + doc.id);
+        System.out.println("content: " + doc.content);
+        System.out.println("creation date: " + doc.getCreationDate());
+        System.out.println("last modification date: " + doc.getLastModificationDate());
     }
 }
