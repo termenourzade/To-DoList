ec2374ae515bdd8a7c699ca8b4e6ee5b9fc8c4d1
terme <termenoorzadeh@gmail.com>
2025-04-11 13:16:41 -0700

    adding step class

--- src/todo/entity/Step.java
+++ src/todo/entity/Step.java
@@ -1,33 +1,21 @@
 package todo.entity;
 
 import db.Entity;
-import db.Trackable;
-
-import java.util.Date;
 
 public class Step extends Entity {
+    public enum Status {
+        NotStarted, Completed
+    }
+
+    public static final int STEP_ENTITY_CODE = 17;
+
     public String title;
     public Status status;
     public int taskRef;
-    public static final int STEP_ENTITY_CODE = 17;
-
-    public enum Status {
-        NotStarted, Completed;
-    }
-
-    public Step(String title, int taskRef) {
-        this.title = title;
-        this.taskRef = taskRef;
-        this.status = Status.NotStarted;
-    }
-
-    @Override
-    public Step clone() throws CloneNotSupportedException {
-        return (Step) super.clone();
-    }
 
     @Override
     public int getEntityCode() {
         return STEP_ENTITY_CODE;
     }
+
 }
